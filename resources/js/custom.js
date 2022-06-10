b135507f34abffd0dea21677a27c6efaece36230
unknown <mohamed.eltaquee@gmail.com>
2022-06-10 15:45:56 +0400

    initial

---
+++ resources/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    
+$("#burger").on("click", function(){
+    $("header nav ul").toggle(200)
+    $("header").toggleClass("header-padding")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
